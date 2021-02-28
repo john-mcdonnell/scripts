d2499dd5792abb0302cfe9e6c4b6a80662b0cacc
John McDonnell <me@home.com>
2021-02-28 14:29:05 +0000

    Latest.

---
+++ nix/workstation/setup/apt/install/install-docker.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#https://docs.docker.com/engine/install/ubuntu/
+
+sudo apt-get -y update
+
+sudo apt-get -y install \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common
+
+curl --request GET --fail --location https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+[ "$(sudo APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=false apt-key fingerprint 0EBFCD88 | grep -c "Docker Release")" != 1 ] && echo "ERROR: Docker repository fingerprint cannot be confirmed."; exit 1
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"Repository
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker "$USER"
--- nix/workstation/setup/apt/install/install-git.sh
+++ nix/workstation/setup/apt/install/install-git.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-#set -x
-
 sudo apt-get -y install git
