7bbe40407e0a3adf1a1aa4a1c10fc1cfb4e0edef
John McDonnell <me@home.com>
2021-03-06 18:41:32 +0000

    Latest.

--- nix/docker/miner/miner-build-imge.sh
+++ nix/docker/miner/miner-build-imge.sh
@@ -3,14 +3,23 @@
 #https://github.com/alexellis/mine-with-docker
 
 #set -x
+set -e
 
 SCRIPT_FOLDER=$(dirname "$0")
 if [[ "$SCRIPT_FOLDER" == "" ]] || [[ "$SCRIPT_FOLDER" == "." ]] || [[ -z "$SCRIPT_FOLDER" ]]; then
   SCRIPT_FOLDER=$(pwd)
 fi
-WORKING_FOLDER=$SCRIPT_FOLDER/mine-with-docker
+SOURCE_FOLDER=$SCRIPT_FOLDER/source
+REPOSITORY_NAME=mine-with-docker
+REPOSITORY_FOLDER="$SOURCE_FOLDER/$REPOSITORY_NAME"
 
-mkdir -p "$WORKING_FOLDER"
-git clone https://github.com/alexellis/mine-with-docker
-cd "$WORKING_FOLDER"/cpu-opt || exit
-docker build -t cpu-opt:latest .
+IMAGE_NAME=${1:-latest}
+IMAGE_TAG=${2:-latest}
+
+[ -d "$SOURCE_FOLDER" ] && sudo rm -R "$SOURCE_FOLDER"
+mkdir -p "$SOURCE_FOLDER"
+cd "$SOURCE_FOLDER" || exit
+git clone "https://github.com/alexellis/$REPOSITORY_NAME"
+cd "$REPOSITORY_FOLDER" || exit
+docker rmi "$IMAGE_NAME":"$IMAGE_TAG"
+docker build -t "$IMAGE_NAME":"$IMAGE_TAG" .
