fab924d12c5e9709d6036d26ba33da35aa30ae62
John McDonnell <me@home.com>
2021-03-06 11:55:12 +0000

    Latest.

--- nix/docker/miner/miner.sh
+++ nix/docker/miner/miner.sh
@@ -7,15 +7,15 @@ CONTAINER_TOTAL=$1
 docker service ps miner >/dev/null 2>&1
 
 if [[ $? -eq 1 ]]; then
-    docker service create \
-        --name miner \
-        alexellis2/cpu-opt:2018-1-2 \
-        ./cpuminer \
-            -a cryptonight \
-            -o stratum+tcp://cryptonight.usa.nicehash.com:3355 \
-            -u 3HSFKUagtL5Z6KFE7gAJ3i9YgWkmaKH6Zb.miner1
+  docker service create \
+    --name miner \
+    alexellis2/cpu-opt:2018-1-2 \
+    ./cpuminer \
+    -a cryptonight \
+    -o stratum+tcp://cryptonight.usa.nicehash.com:3355 \
+    -u 3HSFKUagtL5Z6KFE7gAJ3i9YgWkmaKH6Zb.miner1
 else
-    docker service scale miner="$CONTAINER_TOTAL"
+  docker service scale miner="$CONTAINER_TOTAL"
 fi
 
 docker service logs -f miner
