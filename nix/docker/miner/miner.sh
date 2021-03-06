91c63a6a077fa99f9aea22860afc5216b06e7760
John McDonnell <me@home.com>
2021-03-06 20:42:01 +0000

    Latest.

--- nix/docker/miner/miner.sh
+++ nix/docker/miner/miner.sh
@@ -2,20 +2,27 @@
 
 #https://howchoo.com/bitcoin/mine-bitcoin-on-your-laptop-with-docker#start-mining/
 
-CONTAINER_TOTAL=$1
+#set -x
 
-docker service ps miner >/dev/null 2>&1
-
-if [[ $? -eq 1 ]]; then
-  docker service create \
-    --name miner \
-    alexellis2/cpu-opt:2018-1-2 \
-    ./cpuminer \
-    -a cryptonight \
-    -o stratum+tcp://cryptonight.usa.nicehash.com:3355 \
-    -u 3HSFKUagtL5Z6KFE7gAJ3i9YgWkmaKH6Zb.miner1
-else
-  docker service scale miner="$CONTAINER_TOTAL"
+SCRIPT_FOLDER=$(dirname "$0")
+if [[ "$SCRIPT_FOLDER" == "" ]] || [[ "$SCRIPT_FOLDER" == "." ]] || [[ -z "$SCRIPT_FOLDER" ]]; then
+  SCRIPT_FOLDER=$(pwd)
 fi
 
-docker service logs -f miner
+CPUS=${1:-.5}
+WORKER_NUMBER=${2:-1}
+CONTAINER_NAME=${3:-miner}
+IMAGE_NAME=${4:-kmdgeek/nheqminer}
+IMAGE_TAG=${5:-latest}
+NICE_HASH_USER=${6:-3FQ3Qv4XsY7ADzbCH8tUBZZhp9YMek5DiF}
+NICE_HASH_LOCATION=${7:-equihash.usa.nicehash.com:3357}
+
+[ "$(docker ps | grep --count miner)" -eq 1 ] && docker rm --force "$CONTAINER_NAME"
+docker run --rm --detach \
+  --cpus="$CPUS" \
+  --name "$CONTAINER_NAME" \
+  "$IMAGE_NAME":"$IMAGE_TAG" \
+  /nheqminer \
+  -l "$NICE_HASH_LOCATION" \
+  -u "$NICE_HASH_USER".worker."$WORKER_NUMBER"
+docker logs -f "$CONTAINER_NAME"
